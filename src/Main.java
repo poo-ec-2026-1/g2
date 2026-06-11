48aa9f1e87ab2664b980b4f794041715ff4993ee
PedroJunqueiraUFG <pedrojunqueira@discente.ufg.br>
2026-06-11 10:48:33 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,14 @@
-import model.Produto;
+import view.TelaPrincipal;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Cadastro
-        Produto.adicionarProduto(
-                "Leite",
-                "Leite Integral 1L",
-                6.50,
-                20,
-                "Laticínios"
-        );
+        javax.swing.SwingUtilities.invokeLater(() -> {
 
-        Produto.adicionarProduto(
-                "Refrigerante",
-                "Coca-Cola 2L",
-                12.99,
-                15,
-                "Bebidas"
-        );
+            TelaPrincipal tela = new TelaPrincipal();
+            tela.setVisible(true);
 
-        // Listagem
-        Produto.listarProdutos();
-
-        // Edição
-        Produto.editarProduto(
-                1,
-                "Leite Desnatado",
-                "Leite Desnatado 1L",
-                7.20,
-                18,
-                "Laticínios"
-        );
-
-        // Listagem após edição
-        Produto.listarProdutos();
-
-        // Exclusão
-        Produto.excluirProduto(2);
-
-        // Listagem final
-        Produto.listarProdutos();
+        });
     }
 }
