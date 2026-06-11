2afcca399ef85d1dbc2bdaaa8ed6df9bfcb35154
PedroJunqueiraUFG <pedrojunqueira@discente.ufg.br>
2026-06-11 10:41:39 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,46 @@
+import model.Produto;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Cadastro
+        Produto.adicionarProduto(
+                "Leite",
+                "Leite Integral 1L",
+                6.50,
+                20,
+                "Laticínios"
+        );
+
+        Produto.adicionarProduto(
+                "Refrigerante",
+                "Coca-Cola 2L",
+                12.99,
+                15,
+                "Bebidas"
+        );
+
+        // Listagem
+        Produto.listarProdutos();
+
+        // Edição
+        Produto.editarProduto(
+                1,
+                "Leite Desnatado",
+                "Leite Desnatado 1L",
+                7.20,
+                18,
+                "Laticínios"
+        );
+
+        // Listagem após edição
+        Produto.listarProdutos();
+
+        // Exclusão
+        Produto.excluirProduto(2);
+
+        // Listagem final
+        Produto.listarProdutos();
+    }
 }
