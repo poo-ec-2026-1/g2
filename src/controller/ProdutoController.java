02df96b912a253f52d35d3c744209a85835e1415
PedroJunqueiraUFG <pedrojunqueira@discente.ufg.br>
2026-06-11 10:43:34 -0300

    Update ProdutoController.java

--- src/controller/ProdutoController.java
+++ src/controller/ProdutoController.java
@@ -1,4 +1,10 @@
 package controller;
 
+import java.util.ArrayList;
+import model.Produto;
+
 public class ProdutoController {
+
+    private static ArrayList<Produto> produtos = new ArrayList<>();
+
 }
