19e4c03a15194e97b6d258ef802218236df50701
PedroJunqueiraUFG <pedrojunqueira@discente.ufg.br>
2026-06-25 10:20:41 -0300

    Update CriarTabelas.java

--- src/database/CriarTabelas.java
+++ src/database/CriarTabelas.java
@@ -1,1 +1,19 @@
+package database;
+
+import java.sql.SQLException;
+
+import com.j256.ormlite.table.TableUtils;
+
+import model.Produto;
+
+public class CriarTabelas {
+
+    public static void criar() throws SQLException {
+
+        TableUtils.createTableIfNotExists(
+            DatabaseManager.getConnection(),
+            Produto.class
+        );
+    }
+}
 
