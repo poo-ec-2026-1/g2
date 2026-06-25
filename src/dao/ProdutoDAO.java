949cdcbb74e4ad71445f48c961ac96c9458c42c6
PedroJunqueiraUFG <pedrojunqueira@discente.ufg.br>
2026-06-25 09:38:55 -0300

    Update ProdutoDAO.java

--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -1,1 +1,46 @@
+package dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+
+import database.DatabaseManager;
+import model.Produto;
+
+public class ProdutoDAO {
+
+    private Dao<Produto, Integer> dao;
+
+    public ProdutoDAO() throws SQLException {
+        dao = DaoManager.createDao(
+            DatabaseManager.getConnection(),
+            Produto.class
+        );
+    }
+
+    public void inserir(Produto produto)
+            throws SQLException {
+
+        dao.create(produto);
+    }
+
+    public List<Produto> listar()
+            throws SQLException {
+
+        return dao.queryForAll();
+    }
+
+    public void atualizar(Produto produto)
+            throws SQLException {
+
+        dao.update(produto);
+    }
+
+    public void excluir(int id)
+            throws SQLException {
+
+        dao.deleteById(id);
+    }
+}
