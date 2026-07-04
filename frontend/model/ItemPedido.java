13a000b9823aa23f03f61b0f280f59258ab691c8
Ana Clara <anaclaraborbaa@gmail.com>
2026-07-04 20:16:44 -0300

    Update ItemPedido.java

--- frontend/model/ItemPedido.java
+++ frontend/model/ItemPedido.java
@@ -1,1 +1,25 @@
+package com.castore.model;
+
+public class ItemPedido {
+
+    private final Produto produto;
+    private final int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getSubtotal() {
+        return produto.calcularPreco(quantidade);
+    }
+}
 
